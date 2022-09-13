a1175b85390b3b6870a255985e45e5fd36a99ae5
Tim <tim.fink519@gmail.com>
2022-09-13 19:06:19 -0400

    commit tree b77c86fde46ce3d166518b50d46911b5d5204f97

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution here
+function findMatching(drivers, driverWeAreLookingFor) {
+   const match = drivers.filter(driver => {
+    return driver.toUpperCase() === driverWeAreLookingFor.toUpperCase();
+   })
+
+console.log(driverWeAreLookingFor.charAt(0).toUpperCase() + driverWeAreLookingFor.slice(1));
+   console.log(match);
+   return match;
+}
+
+function fuzzyMatch(drivers, driverWithMatchingLetters) {
+
+    const returnMatchingLetters = drivers.filter(driver => {
+        return driver.substring(0, driverWithMatchingLetters.length) === driverWithMatchingLetters})
+           
+return returnMatchingLetters;
+    
+}
+
+function matchName(drivers, nameToMatch) {
+      const matching = drivers.filter(([drivers, value]) => typeof value === nameToMatch);
+      if(matching) {
+         return drivers.name;
+      }
+};
